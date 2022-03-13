900282d7ad3dcb09e7d46bb93edf96667d494930
dimaslz <me@dimaslz.io>
2022-03-13 18:39:45 +0100

    chore(build): new build version

--- dist/assets/index.ts
+++ dist/assets/index.ts
@@ -37,8 +37,7 @@ RUN %BUILD_COMMAND%
 FROM nginx:alpine
 
 COPY --from=builder /app/dist /usr/share/nginx/html
-
-RUN cat /app/nginx.conf > /etc/nginx/conf.d/default.conf
+COPY --from=builder /app/nginx.conf /etc/nginx/conf.d/default.conf
 
 RUN nginx -t
 RUN nginx -s reload
